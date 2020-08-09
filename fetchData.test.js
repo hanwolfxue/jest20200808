0de1cd730457d102d50857870808a882c0d7bb46
袁开宇 <kaiyu.yuan@hand-china.com>
2020-08-09 22:31:58 +0800

    resolves,rejects,async,await

--- fetchData.test.js
+++ fetchData.test.js
@@ -8,11 +8,37 @@ import { fetchData } from './fetchData.js'
 //   })
 // })
 
-test('fetchData 返回结果为404', () => {
-  expect.assertions(1);
-  return fetchData().catch(e => {
-    expect(e.toString().indexOf('404') > -1).toBe(true)
-  })
-})
+// test('fetchData 返回结果为404', () => {
+//   expect.assertions(1);
+//   return fetchData().catch(e => {
+//     expect(e.toString().indexOf('404') > -1).toBe(true)
+//   })
+// })
 
-// done+promise
+// test('fetchData 返回结果为 { success: true }', () => {
+//   return expect(fetchData()).resolves.toMatchObject({
+//     data: {
+//       success: true
+//     }
+//   })
+// })
+
+// test('fetchData 返回结果为 404', () => {
+//   return expect(fetchData()).rejects.toThrow();
+// })
+
+// test('fetchData 返回结果为 404', async () => {
+//   const response = await fetchData();
+//     expect(response.data).toMatchObject({
+//       success: true
+//     })
+// })
+
+test('fetchData 返回结果为 404', async () => {
+  expect.assertions(1)
+  try {
+    await fetchData();
+  } catch (error) {
+    expect(error.toString()).toMatch(/Error/)
+  }
+})
